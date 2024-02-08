60d8a65fed8e0ba101bea16c95b717799ff01828
Eduardo Maia <edumaia89@outlook.com>
2024-02-08 10:31:44 -0300

    Aula 03

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,8 @@ import { FastifyInstance } from 'fastify'
 import z from 'zod'
 
 import { prisma } from '@/lib/prisma'
+import { redis } from '@/lib/redis'
+import { voting } from '@/utils/voting-pub-sub'
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -23,8 +25,8 @@ export async function voteOnPoll(app: FastifyInstance) {
       const userPreviousVoteOnPoll = await prisma.vote.findUnique({
         where: {
           session_id_poll_id: {
-            session_id,
-            poll_id,
+            session_id: sessionId,
+            poll_id: pollId,
           },
         },
       })
@@ -37,6 +39,17 @@ export async function voteOnPoll(app: FastifyInstance) {
           where: {
             id: userPreviousVoteOnPoll.id,
           },
+        })
+
+        const votes = await redis.zincrby(
+          pollId,
+          -1,
+          userPreviousVoteOnPoll.poll_option_id,
+        )
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.poll_option_id,
+          votes: Number(votes),
         })
       } else if (userPreviousVoteOnPoll) {
         return reply
@@ -62,6 +75,13 @@ export async function voteOnPoll(app: FastifyInstance) {
         poll_id: pollId,
         poll_option_id: pollOptionId,
       },
+    })
+
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
     })
 
     return reply.status(201).send()
